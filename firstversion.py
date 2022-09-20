148f6cc3213d8e253b149f4b76222554495637a7
Mpaulle22 <mah22003@byui.edu>
2022-09-21 01:46:42 +0200

    first attempt

--- firstversion.py
+++ firstversion.py
@@ -1,9 +1,9 @@
 # i define first the grid
 
 
-theGrid = {'1': ' ' , '2': ' ' , '3': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' , 
-            '7': ' ' , '8': ' ' , '9': ' ' }
+theGrid = {'1': ' ', '2': ' ', '3': ' ',
+           '4': ' ', '5': ' ', '6': ' ',
+           '7': ' ', '8': ' ', '9': ' '}
 
 grid_keys = []
 
@@ -19,22 +19,23 @@ def printGrid(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
 
-# here I define the moves 
+# here I define the moves
 # the main function
 def game():
 
     turn = 'X'
     count = 0
-    
+
     for i in range(10):
         printGrid(theGrid)
-        print("It's " + turn +"'s turn," + turn + " Move to which place (1-9)? ")
+        print("It's " + turn + "'s turn," +
+              turn + " Move to which place (1-9)? ")
 
         move = input()
 
         if theGrid[move] == ' ':
             theGrid[move] = turn
-            count+=1
+            count += 1
 
         else:
             print("That place is already filled.\nMove to which place (1-9)? ")
@@ -42,16 +43,16 @@ def game():
 
 # I define here the conditions to define the winner
         if count >= 5:
-            if theGrid['1'] == theGrid['2'] == theGrid['3'] != ' ': #the top line
+            if theGrid['1'] == theGrid['2'] == theGrid['3'] != ' ':  # the top line
                 printGrid(theGrid)
                 print("\nGame Over.\n")
-                print("********" +turn + " won.********")
+                print("********" + turn + " won.********")
                 break
-            
-            elif theGrid['4'] == theGrid['5'] == theGrid['6'] != ' ': #the middle line
+
+            elif theGrid['4'] == theGrid['5'] == theGrid['6'] != ' ':  # the middle line
                 printGrid(theGrid)
                 print("\nGame Over.\n")
-                print("********" +turn + " won.********")
+                print("********" + turn + " won.********")
                 break
 
             elif theGrid['7'] == theGrid['8'] == theGrid['9'] != ' ':  # the bottom line
@@ -90,13 +91,13 @@ def game():
                 print("********" + turn + " won.********")
                 break
 
-#if no one of the player wins, the result is declared as tie
+# if no one of the player wins, the result is declared as tie
         if count == 9:
             print("\nGame Over.\n")
             print("It's a Tie!!")
 
 # this is to change the player after every move
-        if turn =='X':
+        if turn == 'X':
             turn = 'O'
 
         else:
@@ -104,23 +105,18 @@ def game():
 
 # this is to ask the players if they want to play again
 
-    restart =  input("Do you want to play again?(y/n)")
+    restart = input("Do you want to play again?(y/n)")
 
-    
     if restart == "y" or restart == "Y":
         for key in grid_keys:
             theGrid[key] = " "
         game()
     else:
 
-        while restart !="n" or restart !="N":
+        while restart != "n" or restart != "N":
             print("Please enter a correct value")
             restart = input("Do you want to play again?(y/n)")
-        
-            
-        
-        
-        
+
 
 if __name__ == "__main__":
     game()
